cc3daf6a32976112baee3f12de7e1c725d29dc38
Sergey Bondarev <s.bondarev@southbridge.ru>
2018-11-18 23:40:28 +0300

    backup

--- certbot.sh
+++ certbot.sh
@@ -15,9 +15,14 @@ for d in /etc/letsencrypt/live/*; do
   [ -d "$d" ] || continue
   cert=$(basename "$d")
   echo "Create/update sertificate for: $cert"
-  [ ! -d "/etc/letsencrypt/archive/$cert" ] && rm -rf "/etc/letsencrypt/live/$cert"
+  [ ! -d "/etc/letsencrypt/archive/$cert" ] && mv -f "/etc/letsencrypt/live/$cert" "/etc/letsencrypt/live/$cert.bak"
   echo "certbot certonly --noninteractive --no-self-upgrade --webroot --webroot-path /var/www/letsencrypt --agree-tos $email_opt --renew-with-new-domains ${test_cert_opt} -d ${cert}"
   certbot certonly --noninteractive --no-self-upgrade --webroot --webroot-path /var/www/letsencrypt --agree-tos $email_opt --renew-with-new-domains ${test_cert_opt} -d ${cert}
+  if [ -L "/etc/letsencrypt/live/$cert/fullchain.pem" ]; then
+    rm -rf "/etc/letsencrypt/live/$cert.bak"
+  else
+    mv -f "/etc/letsencrypt/live/$cert.bak" "/etc/letsencrypt/live/$cert"
+  fi
 done
 
 nginx -t
