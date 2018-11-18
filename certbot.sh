1a5079b07988a5d8e21f2f704bfb2b936a713fbd
Sergey Bondarev <s.bondarev@southbridge.ru>
2018-11-18 23:30:34 +0300

    fix

--- certbot.sh
+++ certbot.sh
@@ -5,7 +5,7 @@ export TOS="--agree-tos"
 if [ -z "$EMAIL" ]; then
   email_opt="--register-unsafely-without-email"
 else
-  email_opt="--email $EMAIL"
+  email_opt="--email '$EMAIL'"
 fi
 
 typeset test_cert_opt=""
@@ -16,8 +16,8 @@ for d in /etc/letsencrypt/live/*; do
   cert=$(basename "$d")
   echo "Create/update sertificate for: $cert"
   [ ! -d "/etc/letsencrypt/archive/$cert" ] && rm -rf "/etc/letsencrypt/live/$cert"
-echo "certbot certonly --noninteractive --no-self-upgrade --webroot --webroot-path /var/www/letsencrypt --agree-tos $email_opt   --renew-with-new-domains ${test_cert_opt} -d ${cert}"
-  certbot certonly --noninteractive --no-self-upgrade --webroot --webroot-path /var/www/letsencrypt --agree-tos $email_opt   --renew-with-new-domains "${test_cert_opt}" -d "${cert}"
+  echo "certbot certonly --noninteractive --no-self-upgrade --webroot --webroot-path /var/www/letsencrypt --agree-tos $email_opt --renew-with-new-domains ${test_cert_opt} -d ${cert}"
+  certbot certonly --noninteractive --no-self-upgrade --webroot --webroot-path /var/www/letsencrypt --agree-tos $email_opt --renew-with-new-domains ${test_cert_opt} -d ${cert}
 done
 
 nginx -t
